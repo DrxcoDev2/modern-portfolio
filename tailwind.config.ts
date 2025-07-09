73564e131e89adcb274f79710f4cf97ee902d270
DrxcoDev2 <drxcofn2023@gmail.com>
2025-07-09 17:42:57 +0200

    Version 1.1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,14 @@
 import type { Config } from "tailwindcss";
-const svgToDataUri = require("mini-svg-data-uri");
-const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
 
-const config: Config = {
+const svgToDataUri = require("mini-svg-data-uri");
+
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+const config = {
+
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
@@ -10,6 +16,7 @@ const config: Config = {
     "./src/**/*.{ts,tsx}",
     "./data/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
     container: {
       center: true,
@@ -32,7 +39,7 @@ const config: Config = {
           200: "#C1C2D3",
         },
         blue: {
-          100: "#E4ECFF",
+          "100": "#E4ECFF",
         },
         purple: "#CBACF9",
         border: "hsl(var(--border))",
@@ -86,14 +93,13 @@ const config: Config = {
         spotlight: {
           "0%": {
             opacity: "0",
-            transform: "scale(0.5)",
+            transform: "translate(-72%, -62%) scale(0.5)",
           },
           "100%": {
             opacity: "1",
-            transform: "scale(1)",
+            transform: "translate(-50%,-40%) scale(1)",
           },
         },
-
         shimmer: {
           from: {
             backgroundPosition: "0 0",
@@ -144,7 +150,7 @@ const config: Config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        spotlight: "spotlight 2s ease 0.75s 1 forwards",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
         shimmer: "shimmer 2s linear infinite",
         first: "moveVertical 30s ease infinite",
         second: "moveInCircle 20s reverse infinite",
@@ -159,37 +165,34 @@ const config: Config = {
   plugins: [
     require("tailwindcss-animate"),
     addVariablesForColors,
-    function ({ matchUtilities, theme }: { matchUtilities: any; theme: any }) {
+    function ({ matchUtilities, theme }: any) {
       matchUtilities(
         {
-          "bg-grid": (value: string) => ({
+          "bg-grid": (value: any) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="100" height="100" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
           }),
-          "bg-grid-small": (value: string) => ({
+          "bg-grid-small": (value: any) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
           }),
-          "bg-dot": (value: string) => ({
+          "bg-dot": (value: any) => ({
             backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" cx="10" cy="10" r="1.63"/></svg>`
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
             )}")`,
           }),
         },
-        {
-          values: flattenColorPalette(theme("backgroundColor")),
-          type: "color",
-        }
+        { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
       );
     },
   ],
-};
+} satisfies Config;
 
 function addVariablesForColors({ addBase, theme }: any) {
-  const allColors = flattenColorPalette(theme("colors"));
-  const newVars = Object.fromEntries(
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
 
